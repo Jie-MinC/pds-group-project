6234f07b591cea2b9d944a3dde4639e8fd1e7df7
LiTianYeoh <yeohlitian@gmail.com>
2021-12-24 19:47:14 +0800

    Initial template for shiny
    3 tabs and their basic layout.

--- web/server.R
+++ web/server.R
@@ -1,26 +1,83 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# Read Me ------------------------------------------------------------------
+# naming convention:
+# tabName = "tab_{tabname}"
+# id for inputs: "i_{tabname}_{id}"
+# id for outputs: "o_{tabname}_{id}"
 
+
+# Library -----------------------------------------------------------------
 library(shiny)
+library(shinyjs)
+library(dplyr)
+library(ggplot2)
+library(plotly)
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
 
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+# Inititation -------------------------------------------------------------
+#load dataset etc
+numobs<- 800
 
+#normal dataframe
+numvar<-rnorm(numobs, mean = 0, sd=1)
+groupvar<- sample(
+    c("A","B","C"), size=numobs, replace = TRUE
+)
+normdf<-data.frame(groupvar, numvar) %>% 
+    setNames(c("group","value"))
+
+normdistgg<- normdf %>% ggplot(aes(x=value, fill=group)) + theme_bw() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+    geom_density(alpha=0.5)
+
+
+#uniform dataframe
+numvar<-runif(numobs, min = -10, max=10)
+groupvar<- sample(
+    c("A","B","C"), size=numobs, replace = TRUE
+)
+unifdf<-data.frame(groupvar, numvar) %>% 
+    setNames(c("group","value"))
+
+unifdistgg<- unifdf %>% ggplot(aes(x=value, fill=group)) + theme_bw() +
+    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) +
+    geom_density(alpha=0.5)
+
+# Shiny Server ------------------------------------------------------------
+shinyServer(function(input, output, session) {
+    
+    ################ shiny js
+    
+    ################ output in tab_vis
+    output$o_vis_dist<-renderPlotly({
+        baseplot<- switch(input$i_vis_dist_type,
+            'n' = normdistgg,
+            'u' = unifdistgg
+        )
+        
+        ggplotly(baseplot)
     })
+    
+    observeEvent(input$i_vis_direct, {
+        updateTabItems(session,'i_sidetabs',"tab_pred")
+    })
+    
+    
+    ################ output in tab_pred
+    observeEvent(input$i_pred_predbut, {
+        output$o_pred_res<- renderText({
+            "No model predict what"
+        }) #close render result bracket
+    }) #close observe predbit bracket
+    
+    observeEvent(input$i_pred_resetbut, {
+        output$o_pred_res<- renderText({""
+        }) #close render result bracket
+    }) #close observe resetbut bracket
+    
+    ################ output in tab_doc
 
-})
+    ################ output in tab_todo
+    
+}) #close shinyServer bracket
--- web/ui.R
+++ web/ui.R
@@ -1,33 +1,270 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# Read Me ------------------------------------------------------------------
+# naming convention:
+# id for sidebarMenu is "i_sidetabs"
+# tabName = "tab_{tabname}"
+# id for inputs: "i_{tabname}_{id}"
+# id for outputs: "o_{tabname}_{id}"
 
+
+# Library -----------------------------------------------------------------
 library(shiny)
+library(shinydashboard)
+library(dashboardthemes)
+library(shinyjs)
+library(plotly)
+library(leaflet)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+# Initiation --------------------------------------------------------------
+# one time code here
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+
+# Header ------------------------------------------------------------------
+header <- dashboardHeader( 
+    titleWidth = 300,
+    
+    title = "Singapore Flat Resale Price"
+) #close DBheader bracket
+
+
+# Sidebar -------------------------------------------------------------------
+sidebar <- dashboardSidebar(
+    width = 200,
+    sidebarMenu( id="i_sidetabs",
+        menuItem("Visualization", tabName = "tab_vis", 
+                 icon = icon("tasks", lib="glyphicon")
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+        
+        menuItem("Prediction", tabName = "tab_pred",
+                 icon = icon("signal", lib="glyphicon")
+        ),
+        
+        menuItem("Documentation", tabName="tab_doc",
+                 icon = icon("list-alt", lib="glyphicon")
+        ),
+        
+        menuItem("Todo", tabName="tab_todo"
         )
-    )
-))
+        
+    ) #close sidebarMenu bracket
+)# close DBsidebar bracket
+
+
+# Body --------------------------------------------------------------------
+
+body <- dashboardBody(
+    
+    ############################# preparation
+    useShinyjs(),
+    shinyDashboardThemes(theme="poor_mans_flatly"),
+    
+    tabItems(
+        ######################### Visualisation tab        
+        tabItem(
+            "tab_vis",
+            
+            ########### Intro
+            "Welcome! If you are planning to buy a flat in Singapore, ",
+            "then you are at the correct place.",
+            "In this section, you will see some interesting figures and trends ",
+            "on the resale flat prices in Singapore.",
+            br(),
+            
+            ########### Plottings
+            tabBox(
+                height = 500, width = 12,
+                selected = "Test Distribution",
+                
+                tabPanel("Floor Area", 
+                    fluidRow(
+                        column(3,
+                            selectInput("i_vis_area_y", label = "Year",
+                                choices = list("1999","2000","2001")    
+                                )
+                        ), #close control column
+                        column(9,
+                               plotlyOutput("o_vis_area")
+                        ) #close plot column
+                    )# close fluidR bracket
+                ), #close floor area bracket
+                
+                tabPanel("Town",
+                ), #close town bracket
+                
+                tabPanel("Number of storey",
+                ), # close num storey bracket
+                
+                tabPanel("Test Distribution",
+                    fluidRow(
+                        column(3,
+                            selectInput("i_vis_dist_type", label = "Type",
+                                choices = list("Normal" = 'n',
+                                               "Uniform" = 'u') 
+                            )
+                        ), #close control column
+                        column(9,
+                            plotlyOutput("o_vis_dist")
+                        ) #close plot column
+                    )# close fluidR bracket
+                ) # close test distribution bracket
+                
+            ), #close plotting tabbox bracket
+            br(),
+            
+            ########### direct to predict
+            actionButton("i_vis_direct", label = "Click here!"),
+            "to proceed to find out the predicted price of your dream home!"
+        ), #close vis tabItem bracket
+        
+        ######################### Prediction tab        
+        tabItem(
+            "tab_pred",
+            
+            ################ Intro
+            "Let's do some prediction!", br(),
+            "Simply select the flat feature and we will do the remaining job.",
+            br(),
+            
+            ################ feature box
+            box(title = "Flat Features", width = 12,
+                collapsible = FALSE,
+                
+                ############### 1st row for town, flat model and type
+                fluidRow(
+                    column(4, selectInput(
+                        "i_pred_town", label = "Town",
+                        choices = list("A"="a","B"="b"),
+                        selected = "a"
+                    ) #close town input bracket
+                    ), # close town input column bracket
+                    
+                    
+                    column(4,selectInput(
+                        "i_pred_FType", label = "Flat Type",
+                        choices = list("1 Room"="1r","2 Room"="2r"),
+                        selected = "1r"
+                    )#close flat type input bracket
+                    ), #close flat type column bracket
+                    
+                    column(4,selectInput(
+                        "i_pred_FModel", label = "Flat Model",
+                        choices = list("Model A"="Ma","Model B"="Mb"),
+                        selected = "Mb"
+                    )#close flat model input bracket
+                    )#close flat model column bracket
+                    
+                ), #close 2nd row bracket
+                
+                ############### 2nd row for area   
+                
+                fluidRow(
+                    column(4, sliderInput(
+                        "i_pred_area", 
+                                label = HTML(paste0("Floor Area (m", tags$sup("2"), ")")),
+                                width = "300px",
+                                min = 100, max = 200, 
+                                value = 150
+                    ) #close flat type input bracket 
+                    ), #close flat type column bracket
+                    
+                ),# close 2nd row bracket
+                
+                ############### last row for button
+                fluidRow(
+                    column(12, align="right",
+                        actionButton("i_pred_resetbut",
+                            label = "Reset",
+                            width = "100px", color = "blue"
+                        ), #close reset button input bracket
+                        
+                        actionButton("i_pred_predbut",
+                            label = "Predict!",
+                            width = "100px"
+                        ) #close predict button input bracket
+                        
+                       
+                    ) #close predict button column bracket
+                    
+                ),# close 2nd row bracket
+                
+                
+            ),#close flat feature box bracket
+            
+            
+            #################### results box
+            box(title="Results", width = 12,
+                collapsible = FALSE,
+                textOutput("o_pred_res")
+            ) #close results box bracket
+            
+        ), #close pred tabItem bracket
+        
+        ######################### Documentation tab
+        tabItem(
+            "tab_doc",
+            box(title = "Dataset", width = 12,
+                collapsible = FALSE,
+                "The prediction model is trained using", 
+                tags$a(href="https://data.gov.sg/dataset/resale-flat-prices",
+                       "open dataset", target = "_blank"), 
+                "provided by Singapore Housing and Development Board"
+                
+            ),# close dataset box bracket
+            
+            box(title = "Data Attributes", width = 12,
+                collapsible = FALSE,
+                "Hello."
+            ),# close data attr box bracket
+            
+            box(title = "Acknowledgement", width = 12,
+                collapsible = FALSE,
+                "Thank you all"
+            ),# close ack box bracket
+            
+            box(title = "About Us", width = 12,
+                collapsible = FALSE,
+                "We are 5 smart PG students", br(),
+                "Github repo here?"
+            )# close ack box bracket
+            
+        ), #close doc tabItem bracket
+        
+        ######################### Todo tab
+        tabItem(
+            "tab_todo", fluidRow(
+                
+                box(title="Icon choices", width = 5,
+                    collapsible = TRUE,
+                    img(src = "icons.png", height = '250px', width = '400px',
+                        alt = "icon choices")
+                    
+                ), #close icon box bracket
+                
+                box(title="Theme and colors", width = 5,
+                    collapsible = TRUE,
+                    "including change bg color for button like 'reset' ",
+                    "in prediction tab"
+                    
+                )#close theme box bracket
+                
+            )# close fluidrow bracket
+        )#close todo tabItem bracket
+
+        
+        
+                
+    ) #close tabItems bracket
+) #close DBbody bracket
+
+
+
+
+
+
+
+# Shiny UI ----------------------------------------------------------------
+
+
+shinyUI(
+    dashboardPage(header, sidebar, body)
+)
